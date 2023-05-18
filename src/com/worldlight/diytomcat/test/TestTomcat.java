5f3e110469a2f09dd9fb76fbf38c63908104cd33
worldlightyjx <worldlightyjx@qq.com>
2023-05-18 20:01:29 +0800

    Add Test

---
+++ src/com/worldlight/diytomcat/test/TestTomcat.java
@@ -0,0 +1,41 @@
+package com.worldlight.diytomcat.test;
+
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.StrUtil;
+import com.worldlight.diytomcat.util.MiniBrowser;
+import org.jsoup.internal.StringUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.time.Year;
+
+/**
+ * @Author yangjiaxin
+ * @Date 2023/5/18
+ **/
+public class TestTomcat {
+    private static String ip = "127.0.0.1";
+    private static int port = 18080;
+
+    @BeforeClass
+    public static void beforeClass(){
+        if(NetUtil.isUsableLocalPort(port)){
+            System.out.println("Please strat tomcat at port: "+port+" first");
+            System.exit(1);
+        }else{
+            System.out.println("Ready to start test…………");
+
+        }
+    }
+
+    @Test
+    public void testHelloTomcat(){
+
+    }
+
+    private String getContent(String uri){
+        String url = StrUtil.format("http://{}:{}{}",ip,port, uri);
+        String content = MiniBrowser.getContentString(url);
+        return content;
+    }
+}
